100e141f19b3f257b7b4675bc870fe7c60b1daac
smooto <melissasmoot@live.com>
2020-07-22 16:58:03 -0700

    Whoops make sure to save

--- src/containers/MakeRequest/MakeRequest.jsx
+++ src/containers/MakeRequest/MakeRequest.jsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import RequestBar from '../../components/RequestBar/RequestBar';
 
 const MakeRequest = ({ passBack }) => {
   const [url, setUrl] = useState('');
   const [requestType, setRequestType] = useState('GET');
-
-  // const [data, setData] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,13 +12,6 @@ const MakeRequest = ({ passBack }) => {
       .then(res => res.json())
       .then(json => passBack(json));
   };
-
-  // useEffect(() => {
-  //   passBack(data);
-  //   // return () => {
-  //   //   cleanup
-  //   // }
-  // }, [data]);
 
   return <section>
     <h2>New Request</h2>
