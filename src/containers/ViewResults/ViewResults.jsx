13baf08eb88adc381baf22f9d7ae345e65f9764a
smooto <melissasmoot@live.com>
2020-07-22 18:21:38 -0700

    Add formatting package to do it better

--- src/containers/ViewResults/ViewResults.jsx
+++ src/containers/ViewResults/ViewResults.jsx
@@ -1,45 +1,11 @@
 import React from 'react';
+import JSONPretty from 'react-json-pretty';
 
 const ViewResults = ({ data }) => (
   <section>
     <h2>Results</h2>
-    <ul>
-      [<br />
-      {
-        prettify(data)
-        // data.map((object, i) => (
-        //   <li key={'result ' + i}>
-        //     <ul>
-        //       { getKeyValues(object) }
-        //     </ul>
-        //   </li>
-        // ))
-      }
-      ]
-    </ul>
+    <JSONPretty data={data}></JSONPretty>
   </section>
-);
-
-const prettify = (data) => (
-  data.map((object, i) => (
-    <li key={'result ' + i}>
-      &#123;<br />
-      <ul>
-        { getKeyValues(object) }
-      </ul>
-      &#125;
-    </li>
-  ))
-);
-
-const getKeyValues = (object) => (
-  Object.keys(object).map((key, i) => (
-    <li key={i}>
-      <code>
-        {`${key} : ${object[key]},`}
-      </code>
-    </li>
-  ))
 );
 
 export default ViewResults;
